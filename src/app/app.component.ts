9a076712972511a01083266a422b6f3cdeb90f02
MATHEUS HENRICK RIBEIRO NUNES <matheus.hrnunes@senacsp.edu.br>
2026-09-08 20:42:47 -0300

    Site com menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './share/menu/menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'fintechcrud';
+  titulo:string = 'FintechCrud';
+  nome:string = 'MatheusHck';
 }
